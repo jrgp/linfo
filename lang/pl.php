3dce256713550d3e74f7fc90a9c2b10b07990b5d
Joe Gillotti <joe@u13.net>
2010-10-31 06:19:19 +0000

    Really bad attempt at polish translation

---
+++ lang/pl.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * This file is part of Linfo (c) 2010 Joseph Gillotti.
+ * 
+ * Linfo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Linfo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Linfo.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+
+
+defined('IN_INFO') or exit;
+
+/*
+ * Polish translation
+ */
+
+$lang = array(
+	'header' => 'Systemu Ochrony Zdrowia: %s',
+	'core' => 'Rdzeń',
+	'os' => 'OS',
+	'kernel' => 'Jądra',
+	'accessed_ip' => 'Odwiedzona IP',
+	'uptime' => 'Uptime',
+	'hostname' => 'Nazwa komputera',
+	'cpus' => 'CPUs',
+	'load' => 'Load',
+	'processes' => 'Procesy',
+	'threads' => 'Tematy',
+	'total' => 'Razem',
+	'memory' => 'Pamięci',
+	'type' => 'Typ',
+	'used' => 'Używanego',
+	'size' => 'Rozmiar',
+	'physical' => 'Fizycznej',
+	'swap' => 'Swap',
+	'network_devices' => 'Urządzenia Sieciowe',
+	'device_name' => 'Nazwa Urządzenia',
+	'amount_sent' => 'Kwota Wysłane',
+	'amount_received' => 'Otrzymanej Kwoty',
+	'state' => 'państwa',
+	'temps_voltages' => 'Temperatura / Napięcie',
+	'path' => 'Ścieżka',
+	'device' => 'Urządzenie',
+	'value' => 'Wartość',
+	'hardware' => 'Sprzętu',
+	'vendor' => 'Sprzedawca',
+	'drives' => 'Dyski',
+	'name' => 'Nazwa',
+	'reads' => 'Czyta',
+	'writes' => 'Pisze',
+	'size' => 'Rozmiar',
+	'filesystem_mounts' => 'System plików Montaż',
+	'mount_point' => 'Mount Point',
+	'filesystem' => 'plików',
+	'used' => 'Używanego',
+	'free' => 'Pozostałe',
+	'percent_used' => 'Procent Używanego',
+	'raid_arrays' => 'RAID Arrays',
+	'level' => 'Poziom',
+	'status' => 'Pozycja',
+	'devices' => 'Urządzenia',
+	'active' => 'Aktywne',
+	'batteries' => 'Baterie',
+	'charge' => 'Opłaty',
+	'unknown' => 'Nieznany',
+	'footer_app' => '%s Wygenerowane przez w secconds %s.',
+	'none_found' => 'Brak Found',
+	'sound_cards' => 'Karty dźwiękowe',
+	'number' => 'Numer',
+	'card' => 'Karta',
+	'message' => 'Wiadomość',
+	'mount_options' => 'Opcje Montowania',
+	'error_head' => 'Błędy Gromadzenia Danych',
+	'timer' => 'Zegar',
+	'area' => 'Obszar',
+	'time_taken' => 'Czas potrzebny do pobierania',
+	'seconds' => 'sekund',
+	'days' => 'dni',
+	'minutes' => 'minuta',
+	'hours' => 'godzina',
+);
