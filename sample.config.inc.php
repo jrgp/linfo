4d962b0fd1ed00552d91a371acb12a41906e7a4c
Joe Gillotti <joe@u13.net>
2014-01-17 16:21:03 -0500

    Adding "photon" theme, also ripped from 4chan colors

--- sample.config.inc.php
+++ sample.config.inc.php
@@ -13,7 +13,7 @@ $settings['byte_notation'] = 1024; // Either 1024 or 1000; defaults to 1024
 $settings['dates'] = 'm/d/y h:i A (T)'; // Format for dates shown. See php.net/date for syntax
 $settings['language'] = 'en'; // Refer to the lang/ folder for supported lanugages
 $settings['icons'] = true; // simple icons 
-$settings['theme'] = 'default'; // Theme file (layout/theme_$n.css)
+$settings['theme'] = 'default'; // Theme file (layout/theme_$n.css). Look at the contents of the layout/ folder for other themes.
 
 /*
  * Possibly don't show stuff
