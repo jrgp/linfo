782c4d77c95c1e5b8329078398bf589d3e5e5fac
Muhammad Zamroni <halo@matriphe.com>
2017-08-01 14:14:50 +0700

    Add Indonesian (Bahasa Indonesia) translation

---
+++ src/Linfo/Lang/id.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * This file is part of Linfo (c) 2010 Joseph Gillotti.
+ * 
+ * Linfo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Linfo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Linfo. If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+
+namespace Linfo\Lang;
+
+/*
+ * Indonesian (Bahasa Indonesia) translation
+ * By: Muhammad Zamroni <halo@matriphe.com>
+ */
+
+return array(
+    'header' => 'Informasi dan Kesehatan Sistem',
+    'core' => 'Inti',
+    'os' => 'Sistem Operasi',
+    'kernel' => 'Kernel',
+    'accessed_ip' => 'Alamat IP',
+    'uptime' => 'Lama Nyala',
+    'hostname' => 'Nama Host',
+    'cpus' => 'CPU',
+    'phpversion' => 'Versi PHP',
+    'webservice' => 'Server HTTP',
+    'load' => 'Beban',
+    'processes' => 'Proses',
+    'threads' => 'Utas',
+    'total' => 'Total',
+    'memory' => 'Memori',
+    'type' => 'Tipe',
+    'free' => 'Kosong',
+    'used' => 'Digunakan',
+    'size' => 'Ukuran',
+    'physical' => 'Fisik',
+    'swap' => 'SWAP',
+    'network_devices' => 'Perangkat Jaringan',
+    'device_name' => 'Nama Perangkat',
+    'amount_sent' => 'Jumlah Terkirim',
+    'amount_received' => 'Jumlah Diterima',
+    'state' => 'Keadaan',
+    'temps_voltages' => 'Temperatur / Voltase',
+    'path' => 'Path',
+    'device' => 'Perangkat',
+    'value' => 'Nilai',
+    'hardware' => 'Perangkat Keras',
+    'vendor' => 'Vendor',
+    'drives' => 'Drive',
+    'name' => 'Nama',
+    'reads' => 'Baca',
+    'writes' => 'Tulis',
+    'filesystem_mounts' => 'Sistem File Terkait',
+    'mount_point' => 'Titik Kait',
+    'filesystem' => 'Sistem File',
+    'percent_used' => 'Persentase Digunakan',
+    'raid_arrays' => 'Susunan RAID',
+    'level' => 'Level',
+    'status' => 'Status',
+    'devices' => 'Perangkat',
+    'label' => 'Label',
+    'active' => 'Aktif',
+    'batteries' => 'Baterai',
+    'charge' => 'Mengisi Daya',
+    'unknown' => 'Tidak Diketahui',
+    'footer_app' => 'Dihasilkan oleh %s dalam %s detik.',
+    'none_found' => 'Tidak Ditemukan',
+    'sound_cards' => 'Kartu Suara',
+    'number' => 'Nomor',
+    'card' => 'Kartu',
+    'message' => 'Pesan',
+    'from_where' => 'Sumber',
+    'mount_options' => 'Titik Kait',
+    'error_head' => 'Gagal Mengumpulkan Data',
+    'timer' => 'Pewaktu',
+    'area' => 'Area',
+    'time_taken' => 'Waktu Dibutuhkan',
+    'days' => 'hari',
+    'years' => 'tahun',
+    'hours' => 'jam',
+    'minutes' => 'menit',
+    'seconds' => 'detik',
+    'pid' => 'PID',
+    'service' => 'Layanan',
+    'services' => 'Layanan',
+    'memory_usage' => 'Memori Digunakan',
+    'distro' => 'Distribusi',
+    'cpu_arch' => 'Arsitektur',
+    'model' => 'Model',
+    'numLoggedIn' => 'Pengguna Aktif',
+    'virtualization' => 'Virtualisasi',
+    'guest' => 'Tamu',
+    'host' => 'Host',
+    'cpu_usage' => 'Penggunaan Total CPU',
+    'port_speed' => 'Kecepatan Port',
+);
