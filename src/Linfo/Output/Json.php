58bc83a861ff921bb9ae7873bf54954ec7c21227
Joe Gillotti <joe@u13.net>
2017-10-01 16:50:49 -0700

    If we can't encode json, say so

--- src/Linfo/Output/Json.php
+++ src/Linfo/Output/Json.php
@@ -32,10 +32,16 @@ class Json implements Output
             ob_start(function_exists('ob_gzhandler') ? 'ob_gzhandler' : null);
         }
 
+        $encoded = json_encode($this->linfo->getInfo());
+
+        if ($encoded === false) {
+            throw new FatalException('{"error":"Failed generating json: '.(function_exists('json_last_error_msg') ? json_last_error_msg() : json_last_error()).'"}');
+        }
+
         // Give it. Support JSON-P like functionality if the ?callback param looks like a valid javascript
         // function name, including object traversal.
         echo array_key_exists('callback', $_GET) && preg_match('/^[a-z0-9\_\.]+$/i', $_GET['callback']) ?
-          $_GET['callback'].'('.json_encode($this->linfo->getInfo()).')' : json_encode($this->linfo->getInfo());
+          $_GET['callback'].'('.$encoded.')' : $encoded;
 
         // Send it all out
         if (!isset($settings['compress_content']) || $settings['compress_content']) {
