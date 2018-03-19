ac882d93d6ea5bf928de21290a7ec59c5a12552b
Javad Evazzadeh Kakroudi <evazzadeh@users.noreply.github.com>
2018-03-19 09:21:19 +0330

    add Farsi translation (#79)
    * add Persian or Farsi translation

---
+++ src/Linfo/Lang/fa.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * This file is part of Linfo (c) 2010 Joseph Gillotti.
+ *
+ * Linfo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Linfo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Linfo. If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+
+namespace Linfo\Lang;
+
+/*
+ * Farsi translation | https://ermile.com
+ */
+
+return array(
+    'header'            => 'اطلاعات سرور',
+    'core'              => 'هسته',
+    'os'                => 'سیستم‌عامل',
+    'kernel'            => 'کرنل',
+    'accessed_ip'       => 'آی‌پی‌ها',
+    'uptime'            => 'آپ‌تایم',
+    'hostname'          => 'نام هاست',
+    'cpus'              => 'سی‌پی‌یوها',
+    'phpversion'        => 'نسخه پی‌اچ‌پی',
+    'webservice'        => 'سرور اچ‌تی‌تی‌پی',
+    'load'              => 'لود',
+    'processes'         => 'پردازش‌ها',
+    'threads'           => 'موضوعات',
+    'total'             => 'مجموع',
+    'memory'            => 'حافظه',
+    'type'              => 'نوع',
+    'free'              => 'خالی',
+    'used'              => 'استفاده‌شده',
+    'size'              => 'سایز',
+    'physical'          => 'فیزیکی',
+    'swap'              => 'سواپ',
+    'network_devices'   => 'دستگاه‌های شبکه',
+    'device_name'       => 'نام دتستگاه‌ها',
+    'amount_sent'       => 'مقدار ارسال‌شده',
+    'amount_received'   => 'مقدار دریافت‌شده',
+    'state'             => 'حالت',
+    'temps_voltages'    => 'دما / ولتاژ',
+    'path'              => 'آدرس',
+    'device'            => 'دیوایس',
+    'value'             => 'مقدار',
+    'hardware'          => 'سخت‌افزار',
+    'vendor'            => 'عرضه‌کننده',
+    'drives'            => 'درایورها',
+    'name'              => 'نام',
+    'reads'             => 'خواندن‌ها',
+    'writes'            => 'نوشتن‌ها',
+    'filesystem_mounts' => 'فایل سیستم‌های پایه',
+    'mount_point'       => 'نقطه پایه',
+    'filesystem'        => 'فایل سیستم',
+    'percent_used'      => 'درصد استفاده شده',
+    'raid_arrays'       => 'آرایه رید',
+    'level'             => 'سطح',
+    'status'            => 'وضعیت',
+    'devices'           => 'دستگاه‌ها',
+    'label'             => 'برچسب',
+    'active'            => 'فعال',
+    'batteries'         => 'باطری‌ها',
+    'charge'            => 'شارژ',
+    'unknown'           => 'نامشخص',
+    'footer_app'        => 'تولیدشده توسط %s در %s ثانیه.',
+    'none_found'        => 'چیزی یافت نشد',
+    'sound_cards'       => 'کارت صدا',
+    'number'            => 'تعداد',
+    'card'              => 'کارت',
+    'message'           => 'پیام',
+    'from_where'        => 'منبع',
+    'mount_options'     => 'تنظیمات پایه',
+    'error_head'        => 'خطاهای جمع‌آوری داده',
+    'timer'             => 'تایمر',
+    'area'              => 'ناحیه',
+    'time_taken'        => 'زمان به‌دست آوردن',
+    'days'              => 'روز',
+    'years'             => 'سال',
+    'hours'             => 'ساععت',
+    'minutes'           => 'دقیقه',
+    'seconds'           => 'ثانیه',
+    'pid'               => 'پی‌آی‌دی',
+    'service'           => 'سرویس',
+    'services'          => 'سرویس‌ها',
+    'memory_usage'      => 'حافظه استفاده‌شده',
+    'distro'            => 'توزیع',
+    'cpu_arch'          => 'معماری',
+    'model'             => 'مدل',
+    'numLoggedIn'       => 'کاربارن فعال',
+    'virtualization'    => 'مجازی‌سازی',
+    'guest'             => 'مهمان',
+    'host'              => 'هاست',
+    'cpu_usage'         => 'استفاده کلی از سی‌پی‌یو',
+    'port_speed'        => 'سرعت پورت',
+);
