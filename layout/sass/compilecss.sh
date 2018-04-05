1afd5145f86049bf0ae61525e5c17711cb952f15
Joe Gillotti <joe@u13.net>
2018-04-04 23:29:11 -0700

    Move from GPL to MIT

--- layout/sass/compilecss.sh
+++ layout/sass/compilecss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Joe Gillotti - 7/19/14 - GPL
+# Joe Gillotti - 7/19/14 - MIT
 # Compile sass templates to css using http://sass-lang.com/
 for file in theme_*.sass; do
   sass --unix-newlines --style compressed --sourcemap=none $file ../${file%.sass}.css
