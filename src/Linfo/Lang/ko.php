2a88c3cc8e12f2d93d6f5ca99a4e962e43aaf964
woosuk park <bjh13579@gmail.com>
2018-04-29 21:36:46 +0900

    Add Korean Language
    Add Korean Language

---
+++ src/Linfo/Lang/ko.php
@@ -0,0 +1,112 @@
+<?php
+
+/* Linfo
+ *
+ * Copyright (c) 2018 Joe Gillotti
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Linfo\Lang;
+
+/*
+ * Korean translation
+ */
+
+return array(
+    'header' => '시스템 정보 및 상태',
+    'core' => '코어',
+    'os' => 'OS',
+    'kernel' => '커널',
+    'accessed_ip' => '접속한 IP',
+    'uptime' => '구동시간',
+    'hostname' => '호스트 이름',
+    'cpus' => 'CPU',
+    'phpversion' => 'PHP 버전',
+    'webservice' => 'HTTP 서버',
+    'load' => '부하',
+    'processes' => '프로세스',
+    'threads' => '쓰레드',
+    'total' => '총 용량',
+    'memory' => '메모리',
+    'type' => '종류',
+    'free' => '여유',
+    'used' => '사용됨',
+    'size' => '크기',
+    'physical' => '물리',
+    'swap' => '스왑',
+    'network_devices' => '네트워크 장치',
+    'device_name' => '장치 명',
+    'amount_sent' => '보낸 데이터량',
+    'amount_received' => '받은 데이터량',
+    'state' => '상태',
+    'temps_voltages' => '온도 / 전압',
+    'path' => '경로',
+    'device' => '장치',
+    'value' => '값',
+    'hardware' => '하드웨어',
+    'vendor' => '벤더',
+    'drives' => '드라이브',
+    'name' => '이름',
+    'reads' => '읽기',
+    'writes' => '쓰기',
+    'filesystem_mounts' => '파일시스템 마운트 현황',
+    'mount_point' => '마운트 포인트',
+    'filesystem' => '파일 시스템',
+    'percent_used' => '사용된 용량',
+    'raid_arrays' => 'RAID 어레이',
+    'level' => '레벨',
+    'status' => '상태',
+    'devices' => '디바이스',
+    'label' => '라벨',
+    'active' => '액티브',
+    'batteries' => '배터리',
+    'charge' => '충전',
+    'unknown' => '알수없음',
+    'footer_app' => '%s 페이지가 %s 초 안에 생성됨.',
+    'none_found' => '아무것도 찾을수 없음',
+    'sound_cards' => '사운드 카드',
+    'number' => '숫자',
+    'card' => '카드',
+    'message' => '메시지',
+    'from_where' => '소스',
+    'mount_options' => '마운트 옵션',
+    'error_head' => '데이터 수집 오류',
+    'timer' => '타이머',
+    'area' => '에어리어',
+    'time_taken' => '가져오는데 걸린 시간',
+    'days' => '일',
+    'years' => '년',
+    'hours' => '시간',
+    'minutes' => '분',
+    'seconds' => '초',
+    'pid' => 'PID',
+    'service' => '서비스',
+    'services' => '서비스',
+    'memory_usage' => '메모리 사용량',
+    'distro' => '배포판',
+    'cpu_arch' => '아키텍처',
+    'model' => '모델',
+    'numLoggedIn' => '활동 사용자들',
+    'virtualization' => '가상화',
+    'guest' => '게스트',
+    'host' => '호스트',
+    'cpu_usage' => '총 CPU 사용',
+    'port_speed' => '포트 속도',
+);
