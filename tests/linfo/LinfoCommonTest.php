67940f4627cfa60a0db4f1fd3df054fcf2eb3d45
Daniel Kesselberg <mail@danielkesselberg.de>
2018-09-03 18:19:12 +0200

    Replace tabs with 2 spaces, markTestSkipped for skipping tests, []

--- tests/linfo/LinfoCommonTest.php
+++ tests/linfo/LinfoCommonTest.php
@@ -5,27 +5,27 @@ use \Linfo\Linfo;
 
 class CommonTest extends PHPUnit_Framework_TestCase
 {
-    protected static $linfo;
+  protected static $linfo;
 
-    public static function setUpBeforeClass()
-    {
-        self::$linfo = new Linfo();
-    }
+  public static function setUpBeforeClass()
+  {
+    self::$linfo = new Linfo();
+  }
 
-    public static function tearDownAfterClass()
-    {
-        Common::unconfig();
-    }
+  public static function tearDownAfterClass()
+  {
+    Common::unconfig();
+  }
 
   /**
    * @test
    */
   public function arrayAppendString()
   {
-      $strs = array('str1', 'str2', 'str3');
-      $expected = array('str1_suffix', 'str2_suffix', 'str3_suffix');
+    $strs = ['str1', 'str2', 'str3'];
+    $expected = ['str1_suffix', 'str2_suffix', 'str3_suffix'];
 
-      $this->assertEquals(Common::arrayAppendString($strs, '_suffix'), $expected);
+    $this->assertEquals(Common::arrayAppendString($strs, '_suffix'), $expected);
   }
 
   /**
@@ -33,7 +33,7 @@ class CommonTest extends PHPUnit_Framework_TestCase
    */
   public function xmlStringSanitize()
   {
-      $this->assertEquals(Common::xmlStringSanitize('te!@#$%^st'), 'te_st');
+    $this->assertEquals(Common::xmlStringSanitize('te!@#$%^st'), 'te_st');
   }
 
   /**
@@ -41,8 +41,8 @@ class CommonTest extends PHPUnit_Framework_TestCase
    */
   public function anyInArray()
   {
-      $this->assertTrue(Common::anyInArray(array(1, 2, 3, 4, 5), array(5, 6, 7)));
-      $this->assertFalse(Common::anyInArray(array(8, 9, 10), array(11, 12, 3)));
+    $this->assertTrue(Common::anyInArray([1, 2, 3, 4, 5], [5, 6, 7]));
+    $this->assertFalse(Common::anyInArray([8, 9, 10], [11, 12, 3]));
   }
 
   /**
@@ -50,13 +50,13 @@ class CommonTest extends PHPUnit_Framework_TestCase
    */
   public function locateActualPath()
   {
-      $paths = array(
-      LINFO_TESTDIR.'/files/test1.txt',
-      LINFO_TESTDIR.'/files/test2.txt',
-      LINFO_TESTDIR.'/files/test3.txt',
-    );
-      $real = LINFO_TESTDIR.'/files/test2.txt';
-      $this->assertEquals($real, Common::locateActualPath($paths));
+    $paths = [
+        LINFO_TESTDIR . '/files/test1.txt',
+        LINFO_TESTDIR . '/files/test2.txt',
+        LINFO_TESTDIR . '/files/test3.txt',
+    ];
+    $real = LINFO_TESTDIR . '/files/test2.txt';
+    $this->assertEquals($real, Common::locateActualPath($paths));
   }
 
   /**
@@ -64,8 +64,8 @@ class CommonTest extends PHPUnit_Framework_TestCase
    */
   public function getIntFromFile()
   {
-      $file = LINFO_TESTDIR.'/files/intfile.txt';
-      $this->assertEquals(101, Common::getIntFromFile($file));
+    $file = LINFO_TESTDIR . '/files/intfile.txt';
+    $this->assertEquals(101, Common::getIntFromFile($file));
   }
 
   /**
@@ -73,8 +73,8 @@ class CommonTest extends PHPUnit_Framework_TestCase
    */
   public function getVarFromFile()
   {
-      $file = LINFO_TESTDIR.'/files/varfile.php';
-      $this->assertEquals('foo', Common::getVarFromFile($file, 'var'));
+    $file = LINFO_TESTDIR . '/files/varfile.php';
+    $this->assertEquals('foo', Common::getVarFromFile($file, 'var'));
   }
 
   /**
@@ -82,8 +82,8 @@ class CommonTest extends PHPUnit_Framework_TestCase
    */
   public function byteConvert()
   {
-      $this->assertEquals('1000 KiB', Common::byteConvert(1024000));
-      $this->assertEquals('1 GiB', Common::byteConvert(1073741824));
+    $this->assertEquals('1000 KiB', Common::byteConvert(1024000));
+    $this->assertEquals('1 GiB', Common::byteConvert(1073741824));
   }
 
   /**
@@ -91,7 +91,7 @@ class CommonTest extends PHPUnit_Framework_TestCase
    */
   public function secondsConvert()
   {
-      // this is incorrect
+    // this is incorrect
     $this->assertEquals('4 days, 4 hours, 30 seconds', Common::secondsConvert(360000));
 
     // this says '1 hours, 30 seconds' instead. need to find out why...
@@ -103,9 +103,9 @@ class CommonTest extends PHPUnit_Framework_TestCase
    */
   public function getContents()
   {
-      $contents = "lineone\nlinetwo\nline3";
-      $file = LINFO_TESTDIR.'/files/lines.txt';
-      $this->assertEquals($contents, Common::getContents($file));
+    $contents = "lineone\nlinetwo\nline3";
+    $file = LINFO_TESTDIR . '/files/lines.txt';
+    $this->assertEquals($contents, Common::getContents($file));
   }
 
   /**
@@ -113,8 +113,8 @@ class CommonTest extends PHPUnit_Framework_TestCase
    */
   public function getLines()
   {
-      $lines = array("lineone\n", "linetwo\n", "line3\n");
-      $file = LINFO_TESTDIR.'/files/lines.txt';
-      $this->assertEquals($lines, Common::getLines($file));
+    $lines = ["lineone\n", "linetwo\n", "line3\n"];
+    $file = LINFO_TESTDIR . '/files/lines.txt';
+    $this->assertEquals($lines, Common::getLines($file));
   }
 }
