3ba590be5cf40e3a4a47abc5df6bace32d5b530d
Joe Gillotti <joe@u13.net>
2020-11-23 00:30:53 -0800

    Fix tests

--- tests/linfo/parsers/HwpciTest.php
+++ tests/linfo/parsers/HwpciTest.php
@@ -13,11 +13,11 @@ class HwpciTest extends TestCase
   {
     $files = dirname(dirname(dirname(__file__))).'/files';
     $h = new Hwpci('', '', '', false);
-    $resolved = $h->resolve_ids($files.'/pci.ids', ['10de' => true, '1d6a' => true], ['10de1b82' => 1, '1d6a07b1' => 1]);
+    $resolved = $h->resolve_ids($files.'/pci.ids', ['10de' => true, '1d6a' => true], ['10de-1b82' => 1, '1d6a-07b1' => 1]);
 
     $this->assertEquals($resolved, [
-        '10de1b82' => ['NVIDIA Corporation', 'GP104 [GeForce GTX 1070 Ti]'],
-        '1d6a07b1' => ['Aquantia Corp.', 'AQC107 NBase-T/IEEE 802.3bz Ethernet Controller [AQtion]']
+        '10de-1b82' => ['NVIDIA Corporation', 'GP104 [GeForce GTX 1070 Ti]'],
+        '1d6a-07b1' => ['Aquantia Corp.', 'AQC107 NBase-T/IEEE 802.3bz Ethernet Controller [AQtion]']
     ]);
   }
 
@@ -28,11 +28,11 @@ class HwpciTest extends TestCase
   {
     $files = dirname(dirname(dirname(__file__))).'/files';
     $h = new Hwpci('', '', '', false);
-    $resolved = $h->resolve_ids($files.'/usb.ids', ['05ac' => true, '046d' => true], ['05ac1006' => 1, '046dc069' => 1]);
+    $resolved = $h->resolve_ids($files.'/usb.ids', ['05ac' => true, '046d' => true], ['05ac-1006' => 1, '046d-c069' => 1]);
 
     $this->assertEquals($resolved, [
-        '05ac1006' => ['Apple, Inc.', 'Hub in Aluminum Keyboard'],
-        '046dc069' => ['Logitech, Inc.', 'M-U0007 [Corded Mouse M500]']
+        '05ac-1006' => ['Apple, Inc.', 'Hub in Aluminum Keyboard'],
+        '046d-c069' => ['Logitech, Inc.', 'M-U0007 [Corded Mouse M500]']
     ]);
   }
 
